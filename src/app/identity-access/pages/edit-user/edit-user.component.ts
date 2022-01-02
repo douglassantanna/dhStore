dcd5349fb73027539a9a5cf1d0fab88a459da2d5
Douglas Santanna <douglbb1@gmail.com>
2022-01-01 23:36:35 -0300

    trying to fix addTelephone methode bug

--- src/app/identity-access/pages/edit-user/edit-user.component.ts
+++ src/app/identity-access/pages/edit-user/edit-user.component.ts
@@ -1,17 +1,25 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatTable } from '@angular/material/table';
+import { of } from 'rxjs';
 import { KindUser } from '../../enums/KindUser.enum';
-import { NewTelephoneNumber, ViewTelephoneNumber } from '../../interfaces/itelephoneNumber';
+import {
+  NewTelephoneNumber,
+  ViewTelephoneNumber,
+} from '../../interfaces/itelephoneNumber';
 import { NewUser, UpdateUser, ViewUser } from '../../interfaces/iusers';
 
 @Component({
   templateUrl: './edit-user.component.html',
-  styleUrls: ['./edit-user.component.css']
+  styleUrls: ['./edit-user.component.css'],
 })
 export class EditUserComponent implements OnInit {
+  numbersTelephone: NewTelephoneNumber[] = [
+    { number: '123' },
+    { number: '456' },
+  ];
   @ViewChild(MatTable) table!: MatTable<ViewTelephoneNumber>;
-  numbersTelephone: NewTelephoneNumber[] = [{number: '123'}, {number:'456'}];
   columns = ['number', 'option'];
   title: string = '';
   name: string = '';
@@ -23,11 +31,10 @@ export class EditUserComponent implements OnInit {
   newUser: NewUser = {} as NewUser;
   updateUser: UpdateUser = {} as UpdateUser;
   users: ViewUser[] = [];
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public viewUser: ViewUser) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public viewUser: ViewUser) {}
 
-    ngOnInit() {
-      if (this.viewUser) {
+  ngOnInit() {
+    if (this.viewUser) {
       this.title = 'Editar usuário';
       this.name = this.viewUser.name;
       this.email = this.viewUser.email;
@@ -52,15 +59,16 @@ export class EditUserComponent implements OnInit {
   submit(myForm: any) {
     console.log('form value is', myForm.value);
   }
-  addTelephone(){
+  addTelephone() {
     let newTelephoneNumber = {} as NewTelephoneNumber;
-    this.numbersTelephone.push(newTelephoneNumber);
+    if(newTelephoneNumber.number != '')
+      this.numbersTelephone.push(newTelephoneNumber);
+
     this.table.renderRows();
-    console.log(newTelephoneNumber);
+    console.log(this.numbersTelephone);
   }
-  deleteTelephone(){
+  deleteTelephone() {
     this.numbersTelephone.pop();
     this.table.renderRows();
   }
-
 }
