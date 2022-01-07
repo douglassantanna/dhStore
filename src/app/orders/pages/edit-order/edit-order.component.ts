7e049fc1cb80d9fe724795dda417319589043048
Douglas Santanna <douglbb1@gmail.com>
2022-01-07 01:00:09 -0300

    creating order form with reactive forms

--- src/app/orders/pages/edit-order/edit-order.component.ts
+++ src/app/orders/pages/edit-order/edit-order.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './edit-order.component.html',
-  styleUrls: ['./edit-order.component.css']
+  styleUrls: ['./edit-order.component.css'],
 })
 export class EditOrderComponent implements OnInit {
+  title: string = 'Novo pedido';
+  orderForm = new FormGroup({
+    name: new FormControl(''),
+    products: new FormGroup({
+      productName: new FormControl(''),
+      quantity: new FormControl(''),
+      price: new FormControl(''),
+    })
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  updateForm() {
+    // this.orderForm.setValue();
   }
-
+  onSubmit(){
+    console.log(this.orderForm.value);
+  }
 }
